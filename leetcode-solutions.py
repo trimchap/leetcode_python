eb6486992e83d0885c538234cff5427592639dd8
TrimChap <trimchap@trishtunney.com>
2020-08-10 15:42:52 -0700

    Update leetcode-solutions.py

--- leetcode-solutions.py
+++ leetcode-solutions.py
@@ -1,6 +1,6 @@
-#---------------------------------------------
+#--------------------------------------------------------------------------------------------------------------
 # 1. TWO SUMS
-
+#
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 #
@@ -8,8 +8,9 @@
 # Given nums = [2, 7, 11, 15], target = 9,
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
-#---------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
 from typing import List
+
 class Solution:
     def __init__(self):
         return
@@ -23,6 +24,7 @@ class Solution:
                 
 # Test TWO SUMS
 nums = [2,7,11,15]
+target = 9
 solution = Solution()
 print("TWO SUMS: Test - Expected output [0, 1]")
 print(solution.twoSum(nums,9))
